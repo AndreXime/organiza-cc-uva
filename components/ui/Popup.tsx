5cb4578645b5f61fe3c208796497395d10ab0f5c
Andre Ximenes <andreximenes536@gmail.com>
2025-09-12 11:16:09 -0300

    Popup consome diretamente do contexto

--- components/ui/Popup.tsx
+++ components/ui/Popup.tsx
@@ -1,4 +1,20 @@
-export default function Popup({ message }: { message: string }) {
+'use client';
+import { useUI } from '@/context/UIContext';
+import { useEffect } from 'react';
+
+export default function Popup() {
+    const { message, setMessage } = useUI();
+
+    useEffect(() => {
+        if (!message) return;
+
+        const timer = setTimeout(() => {
+            setMessage('');
+        }, 5000);
+
+        return () => clearTimeout(timer);
+    }, [message, setMessage]);
+
     if (!message) return null;
 
     return (
